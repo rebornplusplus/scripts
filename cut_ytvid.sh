03bf7f9a1606571af5befbb39dbeae8e68c14e05
rebornplusplus <rebornplusplus@gmail.com>
2021-07-08 22:18:03 +0600

    Download and Cut YouTube Videos

---
+++ cut_ytvid.sh
@@ -0,0 +1,24 @@
+if [ "$#" -lt 4 ]; then
+	echo "Usage:"
+	echo "	./SCRIPTNAME YT_URL ST_TIME LENGTH FILENAME_WITHOUT_EXTENSION"
+	echo "	ST_TIME and LENGTH in hh:mm:ss format"
+fi
+
+YT_URL=$1
+ST_TIME=$2
+LENGTH=$3
+FILENAME_WITHOUT_EXTENSION=$4
+TEMPLATE=".ac7e1adbc669c240a74d088875e"
+
+youtube-dl "$YT_URL" -o $TEMPLATE
+
+DOWNLOADED_FILE=`find $TEMPLATE*`
+EXT=${DOWNLOADED_FILE##*.}
+FULLFILENAME=$FILENAME_WITHOUT_EXTENSION"."$EXT
+echo $DOWNLOADED_FILE $FULLFILENAME $EXT
+
+# Slower, but A/V sync
+# ffmpeg -i $DOWNLOADED_FILE -ss $ST_TIME -to $LENGTH $FULLFILENAME
+# Faster, but A/V might be slightly out of sync
+ffmpeg -i $DOWNLOADED_FILE -ss $ST_TIME -to $LENGTH -c copy $FULLFILENAME
+rm $DOWNLOADED_FILE
