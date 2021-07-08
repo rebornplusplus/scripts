003254321fb4fb37c42d2ff1cf7be6e5c6d81ed0
rebornplusplus <rebornplusplus@gmail.com>
2021-07-08 22:23:59 +0600

    Download and Cut YouTube Videos
    Updated Usage

--- cut_ytvid.sh
+++ cut_ytvid.sh
@@ -1,12 +1,16 @@
+# Dependencies:
+# 	[youtube-dl](https://youtube-dl.org/)
+#	[ffmpeg](http://ffmpeg.org/)
+
 if [ "$#" -lt 4 ]; then
 	echo "Usage:"
-	echo "	./SCRIPTNAME YT_URL ST_TIME LENGTH FILENAME_WITHOUT_EXTENSION"
-	echo "	ST_TIME and LENGTH in hh:mm:ss format"
+	echo "	./SCRIPTNAME YT_URL ST_TIME EN_TIME FILENAME_WITHOUT_EXTENSION"
+	echo "	ST_TIME and EN_TIME in hh:mm:ss format"
 fi
 
 YT_URL=$1
 ST_TIME=$2
-LENGTH=$3
+EN_TIME=$3
 FILENAME_WITHOUT_EXTENSION=$4
 TEMPLATE=".ac7e1adbc669c240a74d088875e"
 
@@ -18,7 +22,7 @@ FULLFILENAME=$FILENAME_WITHOUT_EXTENSION"."$EXT
 echo $DOWNLOADED_FILE $FULLFILENAME $EXT
 
 # Slower, but A/V sync
-# ffmpeg -i $DOWNLOADED_FILE -ss $ST_TIME -to $LENGTH $FULLFILENAME
+# ffmpeg -i $DOWNLOADED_FILE -ss $ST_TIME -to $EN_TIME $FULLFILENAME
 # Faster, but A/V might be slightly out of sync
-ffmpeg -i $DOWNLOADED_FILE -ss $ST_TIME -to $LENGTH -c copy $FULLFILENAME
+ffmpeg -i $DOWNLOADED_FILE -ss $ST_TIME -to $EN_TIME -c copy $FULLFILENAME
 rm $DOWNLOADED_FILE
